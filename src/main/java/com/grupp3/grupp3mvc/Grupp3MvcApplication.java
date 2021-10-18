31177f1f02a1eed7a9261375acee049a0deaa0a0
JimNotCarry <59293465+JimNotCarry@users.noreply.github.com>
2021-10-18 14:03:31 +0200

    Initial commit

---
+++ src/main/java/com/grupp3/grupp3mvc/Grupp3MvcApplication.java
@@ -0,0 +1,13 @@
+package com.grupp3.grupp3mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupp3MvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupp3MvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grupp3/grupp3mvc/Grupp3MvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp3.grupp3mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupp3MvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
