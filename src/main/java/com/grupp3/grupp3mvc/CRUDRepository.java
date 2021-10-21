ab675cc732ecaf34f6352cc436fb33773de65557
awate99 <awate11@live.se>
2021-10-21 11:10:48 +0200

    Pair progamming med gruppen skickar upp så vi kan fortsätta från någon annan! Bra kört btw

--- src/main/java/com/grupp3/grupp3mvc/CRUDRepository.java
+++ src/main/java/com/grupp3/grupp3mvc/CRUDRepository.java
@@ -5,3 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CRUDRepository extends JpaRepository<Product, Integer> {
 }
+//Product står för våran pojo och kopplar vi till våran databas
--- src/main/java/com/grupp3/grupp3mvc/Grupp3MvcApplication.java
+++ src/main/java/com/grupp3/grupp3mvc/Grupp3MvcApplication.java
@@ -12,4 +12,5 @@ public class Grupp3MvcApplication {
 
 
 
-}
+
+ }
